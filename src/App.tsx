3d36be798fcd401b35a4f9c3ab859745da107758
Andreas Fagerlund <mcshaffa@gmail.com>
2025-02-19 16:09:31 +0100

    added link to in aside

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-// import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 export default function MediaPlayerLayout() {
   return (
     <div className="flex flex-col text-slate-50">
-      <header className="bg-slate-950 text-4xl px-4 py-2 justify-between">
+      <header className="bg-green-900 text-4xl px-8 py-4 justify-between">
         Header
       </header>
+
       <div className="flex gap-2">
-        <aside className="bg-slate-800 text-4xl p-4 rounded">Sidebar</aside>
-        <main className="bg-slate-700 rounded p-4 flex-1"></main>
+        <aside className="bg-green-900 text-4xl p-8 rounded-br-3xl">
+          <nav className="flex flex-col gap-6">
+            <Link to="/">Hem</Link>
+            <Link to="/depression">Depression</Link>
+            <Link to="/anxiety">Ångest</Link>
+            <Link to="/burnout">Utbrändhet</Link>
+            <Link to="/resources">Resurser</Link>
+          </nav>
+        </aside>
+
+        <main className="p-4 flex-1">
+          <Outlet />
+        </main>
       </div>
     </div>
   );
