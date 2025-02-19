a5b14e217d3274050bdff1ad2f1cdad4c5da502e
Andreas Fagerlund <mcshaffa@gmail.com>
2025-02-19 16:55:12 +0100

    Added empty files to pages and components

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+// import { Link } from "react-router";
+// import { Artist } from "../data";
+
+// interface Props {
+//   artist: Artist;
+// }
+
+// export default function ArtistCard(props: Props) {
+//   return (
+//     <Link to={`artists/${props.artist.slug}/${props.artist.id}`} className="p-2 hover:bg-slate-800 rounded">
+//       <img className="rounded-full" src={props.artist.imageUrl} />
+//       <h3>{props.artist.name}</h3>
+//       <span className="text-sm text-slate-300">Artists</span>
+//     </Link>
+//   );
+// }
---
+++ src/pages/Depression.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <h1 className="text-purple-600 text-4xl">Depression</h1>;
+}
