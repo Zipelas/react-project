b6ea4ff6b12ddcbb8ce5c0a41dd62c8685f2012b
Andreas Fagerlund <mcshaffa@gmail.com>
2025-02-19 19:44:10 +0100

    added h1 and text

--- src/pages/Stress.tsx
+++ src/pages/Stress.tsx
@@ -1,3 +1,17 @@
 export default function App() {
-  return <h1 className="text-purple-600 text-4xl">Stress</h1>;
+  return (
+    <div className="bg-green-500 min-h-screen flex justify-center p-6 rounded-xl">
+      <div className="bg-white max-w-xl p-6 rounded-lg mt-10">
+        <h1 className="text-green-900 text-4xl mb-4">Stress</h1>
+        <p className="text-green-900">
+          Stress är en reaktion på en yttre påfrestning. Det kan vara en fysisk
+          eller psykisk påfrestning. Stress kan vara positivt och negativt.
+          Positiv stress kan vara...{" "}
+          <a href="https://www.1177.se/sjukdomar--besvar/psykiska-sjukdomar-och-besvar/stress/">
+            Läs mer
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
