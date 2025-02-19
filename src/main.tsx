9477b56254bdc2f64f8cf6e4f095b0fa90216eaa
Andreas Fagerlund <mcshaffa@gmail.com>
2025-02-19 17:25:26 +0100

    added route and import

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,27 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
+import Anxiety from "./pages/Anxiety.tsx";
+import Burnout from "./pages/Burnout.tsx";
+import Depression from "./pages/Depression.tsx";
+import Home from "./pages/Home.tsx";
+import NotFound from "./pages/NotFound.tsx";
+import Resources from "./pages/Resources.tsx";
+import Stress from "./pages/Stress.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} /> {/* Startar med Home */}
+          <Route path="depression" element={<Depression />} />
+          <Route path="anxiety" element={<Anxiety />} />
+          <Route path="stress" element={<Stress />} />
+          <Route path="burnout" element={<Burnout />} />
+          <Route path="resources" element={<Resources />} />
+          <Route path="*" element={<NotFound />} /> {/* 404-sida */}
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
