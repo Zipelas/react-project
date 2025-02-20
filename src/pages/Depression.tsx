6e99390c60f107463383556d0138061c85c7acb0
Andreas Fagerlund <mcshaffa@gmail.com>
2025-02-20 16:13:58 +0100

    adde InfoCard with text and links

--- src/pages/Depression.tsx
+++ src/pages/Depression.tsx
@@ -1,3 +1,16 @@
-export default function App() {
-  return <h1 className="text-purple-600 text-4xl">Depression</h1>;
+import InfoCard from "../components/InfoCard";
+
+export default function Depression() {
+  return (
+    <div className="bg-green-500 min-h-screen flex justify-center p-6 rounded-xl">
+      <InfoCard
+        title="Depression"
+        ingress="Du kan ha fått en depression om du under en längre tid känner dig nedstämd, trött och inte har någon ork. Det kan också vara att du känner att du har tappat lusten även till sådant som du brukar tycka om att göra. Det är viktigt att inte vänta för länge med att söka hjälp."
+        text="Att känna sig ledsen eller nedstämd är vanligt och händer alla ibland. Det är vanliga reaktioner som oftast går över av sig själv. Här kan du läsa mer om vad derpression är, symtom och när du ska söka vård."
+        link="https://www.1177.se/Vastra-Gotaland/sjukdomar--besvar/psykiska-sjukdomar-och-besvar/depression/depression/"
+        imageUrl="../src/assets/depression.jpg"
+        altText="Person som hanterar stress"
+      />
+    </div>
+  );
 }
