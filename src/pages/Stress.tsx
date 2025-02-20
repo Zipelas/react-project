77bed4555f3f5df604dc5bb6990046924f8cede6
Andreas Fagerlund <mcshaffa@gmail.com>
2025-02-20 16:15:30 +0100

    changed text and links in <InfoCard/>

--- src/pages/Stress.tsx
+++ src/pages/Stress.tsx
@@ -5,9 +5,9 @@ export default function Stress() {
     <div className="bg-green-500 min-h-screen flex justify-center p-6 rounded-xl">
       <InfoCard
         title="Stress"
-        ingress="Stress är en naturlig reaktion i kroppen som uppstår när vi upplever hot eller fara."
-        text="Stress kan vara både positivt och negativt. Positiv stress kan hjälpa oss att prestera bättre i vissa situationer, medan negativ stress kan leda till ohälsa och sjukdom."
-        link="https://www.1177.se/Vastra-Gotaland/liv--halsa/stresshantering-och-somn/stress/https://www.1177.se/sjukdomar--besvar/stress-och-angest/"
+        ingress="Stress kan ge dig extra energi när du har bråttom eller måste klara något viktigt. Men du kan må dåligt om du är stressad för länge eller för ofta. Det är viktigt att få pauser från stress."
+        text="Stress kan påverka kroppen på många sätt. Det kan visa sig på olika sätt för olika personer. Här kan du läsa mer om vad stress är, tecken på att du mår dåligt av stress och när du ska söka vård."
+        link="https://www.1177.se/Vastra-Gotaland/liv--halsa/stresshantering-och-somn/stress/"
         imageUrl="../src/assets/stress.jpg"
         altText="Person som hanterar stress"
       />
