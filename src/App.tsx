de6cbb5c4cc80b6ee07baaec616ad0252edf89b5
Andreas Fagerlund <mcshaffa@gmail.com>
2025-02-23 14:37:54 +0100

    added Header and removed header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router";
 import Aside from "./components/Aside";
+import Header from "./components/Header";
 
 export default function MediaPlayerLayout() {
   return (
     <div className="flex flex-col text-slate-50">
-      <header className="flex items-center justify-center bg-green-900 text-3xl px-8 py-4 font-bold">
-        <span className="mr-2">❤</span> Mental hälsa
-      </header>
-
+      <Header />
       <div className="flex gap-2">
         <Aside />
 
