92e4821631b8bfb63047947955fe4ba2fed55b81
Andreas Fagerlund <mcshaffa@gmail.com>
2025-02-23 14:42:14 +0100

    made Emergency

---
+++ src/pages/Emergency.tsx
@@ -0,0 +1,16 @@
+import InfoCard from "../components/InfoCard";
+
+export default function Emergency() {
+  return (
+    <div className="bg-red-600 min-h-screen flex justify-center p-6 rounded-xl">
+      <InfoCard
+        title="Akut"
+        ingress="Stress kan ge dig extra energi när du har bråttom eller måste klara något viktigt. Men du kan må dåligt om du är stressad för länge eller för ofta. Det är viktigt att få pauser från stress."
+        text="Stress kan påverka kroppen på många sätt. Det kan visa sig på olika sätt för olika personer. Här kan du läsa mer om vad stress är, tecken på att du mår dåligt av stress och när du ska söka vård."
+        link="https://www.1177.se/Vastra-Gotaland/liv--halsa/stresshantering-och-somn/stress/"
+        imageUrl="../src/assets/stress.jpg"
+        altText="En man som är stressad"
+      />
+    </div>
+  );
+}
