a5f42304ef9a96a8b573dcc4b23890db9fb4097a
Andreas Fagerlund <mcshaffa@gmail.com>
2025-02-23 15:15:41 +0100

    changed content

--- src/pages/Emergency.tsx
+++ src/pages/Emergency.tsx
@@ -5,11 +5,11 @@ export default function Emergency() {
     <div className="bg-red-600 min-h-screen flex justify-center p-6 rounded-xl">
       <InfoCard
         title="Akut"
-        ingress="Stress kan ge dig extra energi när du har bråttom eller måste klara något viktigt. Men du kan må dåligt om du är stressad för länge eller för ofta. Det är viktigt att få pauser från stress."
-        text="Stress kan påverka kroppen på många sätt. Det kan visa sig på olika sätt för olika personer. Här kan du läsa mer om vad stress är, tecken på att du mår dåligt av stress och när du ska söka vård."
-        link="https://www.1177.se/Vastra-Gotaland/liv--halsa/stresshantering-och-somn/stress/"
-        imageUrl="../src/assets/stress.jpg"
-        altText="En man som är stressad"
+        ingress="Här hittar du länkar som hjälper dig om läget är akut"
+        text="1177"
+        link="https://www.1177.se/"
+        imageUrl="../src/assets/emergency.jpg"
+        altText="Emergency-skylt"
       />
     </div>
   );
