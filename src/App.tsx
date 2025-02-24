260da5babbd6ccda65330ed9724f294f1b77edb8
Andreas Fagerlund <mcshaffa@gmail.com>
2025-02-24 18:26:37 +0100

    added a menu and a scroll. closes when clicked outside, on close icon and on link. Added some styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,25 @@ export default function App() {
   const [isCrisisMode, setIsCrisisMode] = useState(false);
 
   return (
-    <div className="flex flex-col text-slate-50">
-      <Header bgColor={isCrisisMode ? "bg-red-600" : "bg-green-900"} />
-      <div className="flex gap-2">
-        <Aside
-          bgColor={isCrisisMode ? "bg-red-600" : "bg-green-900"}
-          setIsCrisisMode={setIsCrisisMode}
-          isCrisisMode={isCrisisMode}
-        />
+    <div className="flex flex-col text-slate-50 min-h-screen">
+      {/* Header hanterar hamburgermenyn */}
+      <Header
+        bgColor={isCrisisMode ? "bg-red-600" : "bg-green-900"}
+        setIsCrisisMode={setIsCrisisMode}
+        isCrisisMode={isCrisisMode}
+      />
 
+      <div className="flex flex-1 gap-2">
+        {/* Visa `Aside.tsx` ENDAST på stora skärmar */}
+        <div className="hidden md:block">
+          <Aside
+            bgColor={isCrisisMode ? "bg-red-600" : "bg-green-900"}
+            setIsCrisisMode={setIsCrisisMode}
+            isCrisisMode={isCrisisMode}
+          />
+        </div>
+
+        {/* Huvudinnehåll */}
         <main className="flex-1 ml-4">
           <Outlet />
         </main>
