83e8ba846159a56e1b5030e676342ae550d365a5
Andreas Fagerlund <mcshaffa@gmail.com>
2025-02-25 20:46:42 +0100

    trimed Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,7 +40,7 @@ export default function Header({
       <div
         className={`flex items-center ${bgColor} text-white text-3xl px-8 py-4 font-bold rounded-3xl m-1 w-full`}
       >
-        {/* Mobil-layout (< 768px) → Text till vänster, Hamburgermeny till höger */}
+        {/* Mobil-layout (< 768px) */}
         <div className="flex w-full md:hidden justify-between items-center">
           <h1 className="text-2xl font-bold">❤ Mental hälsa</h1>
 
@@ -54,20 +54,20 @@ export default function Header({
           </button>
         </div>
 
-        {/* Desktop-layout (> 768px) → Texten är centrerad */}
+        {/* Desktop-layout (> 768px) - Texten är centrerad */}
         <h1 className="hidden md:block w-full text-center text-2xl font-bold">
           ❤ Mental hälsa
         </h1>
       </div>
 
-      {/* Hamburgermenyn 50% bred även på 330px, full scrollbar vid behov */}
+      {/* Scrollbar vid behov */}
       <div
         id="mobile-menu"
         className={`fixed top-[90px] right-0 h-[80vh] w-1/2 min-w-[165px] max-w-[330px] bg-green-900 text-white p-6 shadow-lg rounded-3xl mr-1 overflow-y-auto md:hidden transition-transform transform ${
           menuOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
-        {/* Stäng-knapp (X-ikon) */}
+        {/* (X-ikon) */}
         <button
           className="absolute top-4 right-4 text-white text-3xl"
           onClick={() => setMenuOpen(false)}
