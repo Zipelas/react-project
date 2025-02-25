95f5f1205d0e8a8652ecc4ff8a5364ac8630ccaa
Andreas Fagerlund <mcshaffa@gmail.com>
2025-02-25 17:21:59 +0100

    changed imageUrl from local to Pexels

--- src/pages/Stress.tsx
+++ src/pages/Stress.tsx
@@ -8,7 +8,7 @@ export default function Stress() {
         ingress="Stress kan ge dig extra energi när du har bråttom eller måste klara något viktigt. Men du kan må dåligt om du är stressad för länge eller för ofta. Det är viktigt att få pauser från stress."
         text="Stress kan påverka kroppen på många sätt. Det kan visa sig på olika sätt för olika personer. Här kan du läsa mer om vad stress är, tecken på att du mår dåligt av stress och när du ska söka vård."
         link="https://www.1177.se/Vastra-Gotaland/liv--halsa/stresshantering-och-somn/stress/"
-        imageUrl="../src/assets/stress.jpg"
+        imageUrl="https://images.pexels.com/photos/897817/pexels-photo-897817.jpeg?auto=compress&cs=tinysrgb&w=400"
         altText="En man som är stressad"
       />
     </div>
